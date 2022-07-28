b8b97433d1a993aec5857bac794ba163c85571f1
spectre2727 <gsoldano27@aol.com>
2022-07-28 15:23:04 +0200

    modified: git.sh new file: node18.7.0.sh

--- git.sh
+++ git.sh
@@ -1,4 +1,5 @@
-echo root | sudo -S add-apt-repository ppa:git-core/ppa
+echo root | sudo -S echo ""
+sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt update
 sudo apt install git -y
 
@@ -11,5 +12,11 @@ ssh-add ~/.ssh/id_ed25519
 
 sudo apt install gh -y
 gh auth login
+
+#What account do you want to log into? GitHub.com
+#What is your preferred protocol for Git operations? SSH
+#Upload your SSH public key to your GitHub account? Skip
+#How would you like to authenticate GitHub CLI? Login with a web browser
+
 gh auth refresh -h github.com -s admin:public_key
-gh ssh-key add ~/.ssh/id_ed25519.pub -t "n56vv"
+gh ssh-key add ~/.ssh/id_ed25519.pub -t "dell3510"
---
+++ node18.7.0.sh
@@ -0,0 +1,40 @@
+wget https://nodejs.org/dist/v18.7.0/node-v18.7.0-linux-x64.tar.xz
+if [ -s *.tar.xz ]; then
+  echo -e "Download completed.\n"
+else
+  echo -e "File not downloaded.\n"
+  exit 1
+fi
+
+tar -xf node*.tar.xz
+if [ -d $PWD/node*/ ]; then
+  rm node*.tar.xz
+  echo -e "Extraction completed.\n"
+else
+  echo -e "Extraction not performed.\n"
+  exit 1
+fi
+
+nodefolder=$(basename $PWD/node*/) && echo -e "$nodefolder \n"
+
+echo root | sudo -S echo ""
+
+sudo cp -R node*/ /opt && sudo rm -R node*/ && echo ""
+if [ -d /opt/$nodefolder ]; then
+  echo -e "Folder moved to /opt directory.\n"
+else
+  echo -e "Something wrong when moving the directory.\n"
+  exit 1
+fi
+
+sudo echo "" >> ~/.profile
+sudo echo export "NODE_HOME=/opt/$nodefolder" >> ~/.profile
+sudo echo 'export PATH=$NODE_HOME/bin:$PATH' >> ~/.profile
+if grep -q "export NODE_HOME" ~/.profile; then
+  if grep -q 'export PATH=$NODE_HOME' ~/.profile; then
+    echo -e "Environment variable added.\n"
+  else
+    echo -e "Something wrong with adding environment variable.\n"
+    exit 1
+  fi
+fi
