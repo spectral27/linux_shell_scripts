7bf001e920ae134c558748db07c470184af3cb5f
spectre2727 <gsoldano27@outlook.com>
2023-02-22 21:41:03 +0100

    Update

--- git.sh
+++ git.sh
@@ -1,12 +1,11 @@
-echo root | sudo -S echo ""
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt update
 sudo apt install git -y
 
 git config --global user.name "spectre2727"
-git config --global user.email "gsoldano27@aol.com"
+git config --global user.email "gsoldano27@outlook.com"
 
-ssh-keygen -t ed25519 -C "gsoldano27@aol.com"
+ssh-keygen -t ed25519 -C "gsoldano27@outlook.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
@@ -20,3 +19,4 @@ gh auth login
 
 gh auth refresh -h github.com -s admin:public_key
 gh ssh-key add ~/.ssh/id_ed25519.pub -t "dell3510"
+
