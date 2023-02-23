3b206fe657b5d355458c38556e57df0015df21fb
spectre2727 <gsoldano27@outlook.com>
2023-02-24 00:15:08 +0100

    Update

---
+++ 1.sh
@@ -0,0 +1,24 @@
+sh java17.sh
+returncode=$? && echo "${returncode}\n"
+
+if [ ${returncode} -gt 0 ]; then
+  echo "Java installation error"
+  exit 1
+fi
+
+sh gradle.sh
+returncode=$? && echo -e "${returncode}\n"
+
+if [ ${returncode} -gt 0 ]; then
+  echo "Gradle installation error\n"
+  exit 1
+fi
+
+sh maven3.sh
+returncode=$? && echo -e "${returncode}\n"
+
+if [ ${returncode} -gt 0 ]; then
+  echo "Gradle installation error\n"
+  exit 1
+fi
+
