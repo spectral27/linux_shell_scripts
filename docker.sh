e1d68eb9d98ecaedfe09eb47b065e6f2dd1f9b71
spectre2727 <gsoldano27@outlook.com>
2023-02-23 23:34:00 +0100

    Update

--- docker.sh
+++ docker.sh
@@ -1,16 +1,20 @@
-echo root | sudo -S echo "" >> /dev/null
+if [ $# -lt 1 ]; then
+  echo "Please type Ubuntu codename as argument (e.g. jammy)"
+  echo 1
+fi
 
 sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo apt-get install -y ca-certificates curl gnupg
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
-https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] \
+https://download.docker.com/linux/ubuntu $1 stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
+
--- intellij.sh
+++ intellij.sh
@@ -1,6 +1,19 @@
-wget https://download.jetbrains.com/idea/ideaIC-2022.1.2.tar.gz
+version=2022.3.2
+wget https://download.jetbrains.com/idea/ideaIC-${version}.tar.gz
+
 tar -xf idea*.tar.gz
 rm idea*.tar.gz
+
 ijfolder=$(basename $PWD/idea*/)
-mv $ijfolder ~
-~/$ijfolder/bin/idea.sh
+
+if [ ! -d /inst/ ]; then
+  echo -e "/inst/ directory not found, creating it\n"
+  sudo mkdir /inst/
+  sudo chmod a+rwx /inst/
+else
+  echo -e "/inst/ directory found\n"
+fi
+
+mv $ijfolder /inst/
+/inst/$ijfolder/bin/idea.sh
+
