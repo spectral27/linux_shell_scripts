9d6d5e268aab431ab9f3b9d1a0168179e9999f2c
spectre2727 <gsoldano27@outlook.com>
2023-02-28 00:20:34 +0100

    Update

--- git.sh
+++ git.sh
@@ -28,11 +28,15 @@ eval "$(ssh-agent)"
 ssh-add /home/$USER/.ssh/${keyname}
 
 type -p curl > /dev/null || sudo apt install curl -y
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+
+archsignedby="[arch=amd64 signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg]"
+stable="https://cli.github.com/packages stable main"
+
+echo "deb $archsignedby $stable" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh -y
 
 gh auth login
 
